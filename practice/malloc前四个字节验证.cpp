dfcb310368b32cd55f2a9190ea3b91885ecb6d25
EdgerZ <382674578@qq.com>
2018-11-03 13:18:01 +0800

    增加对malloc的验证

---
+++ practice/malloc前四个字节验证.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <string.h>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+void test1()
+{
+	char* p = (char*)malloc(1024);
+	printf("分配的位置在：%p\n", p);
+	p -= 12;
+	printf("向前偏移4个字节后的位置：%p\n", p);
+	printf("此时强转之后的值：%d\n", *(int *)p);
+	return;
+}
+
+
+int main()
+{
+	test1();
+
+
+	system("pause");
+	return 0;
+}
--- practice/二叉树的遍历.cpp
+++ practice/二叉树的遍历.cpp
@@ -1,80 +1,80 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <cstring>
-#include <string.h>
-#include <cmath>
-#include <ctime>
-using namespace std;
-
-typedef struct binaryTree
-{
-	char flag;
-	struct binaryTree* lNode;
-	struct binaryTree* rNode;
-}binTree;
-
-void recursionF(binTree* node)
-{
-	if (node == NULL)
-	{
-		return;
-	}
-	printf("%c ", node->flag);
-	recursionF(node->lNode);
-	recursionF(node->rNode);
-}
-
-void recursionM(binTree* node)
-{
-	if (node == NULL)
-	{
-		return;
-	}
-	recursionM(node->lNode);
-	printf("%c ", node->flag);
-	recursionM(node->rNode);
-}
-
-void recursionL(binTree* node)
-{
-	if (node == NULL)
-	{
-		return;
-	}
-	recursionL(node->lNode);
-	recursionL(node->rNode);
-	printf("%c ", node->flag);
-}
-
-
-int main()
-{
-	binTree node[7];
-	for (int i = 0; i < 7; i++)
-	{
-		node[i].flag = 'A' + i;
-		node[i].lNode = NULL;
-		node[i].rNode = NULL;
-	}
-	node[0].lNode = &node[1];
-	node[1].lNode = &node[2];
-	node[1].rNode = &node[3];
-	node[0].rNode = &node[4];
-	node[4].lNode = &node[5];
-	node[4].rNode = &node[6];
-
-	cout << "先序遍历：";
-	recursionF(node);
-	putchar(10);
-
-	cout << "中序遍历：";
-	recursionM(node);
-	putchar(10);
-
-	cout << "后序遍历：";
-	recursionL(node);
-	putchar(10);
-
-	system("pause");
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <cstring>
+//#include <string.h>
+//#include <cmath>
+//#include <ctime>
+//using namespace std;
+//
+//typedef struct binaryTree
+//{
+//	char flag;
+//	struct binaryTree* lNode;
+//	struct binaryTree* rNode;
+//}binTree;
+//
+//void recursionF(binTree* node)
+//{
+//	if (node == NULL)
+//	{
+//		return;
+//	}
+//	printf("%c ", node->flag);
+//	recursionF(node->lNode);
+//	recursionF(node->rNode);
+//}
+//
+//void recursionM(binTree* node)
+//{
+//	if (node == NULL)
+//	{
+//		return;
+//	}
+//	recursionM(node->lNode);
+//	printf("%c ", node->flag);
+//	recursionM(node->rNode);
+//}
+//
+//void recursionL(binTree* node)
+//{
+//	if (node == NULL)
+//	{
+//		return;
+//	}
+//	recursionL(node->lNode);
+//	recursionL(node->rNode);
+//	printf("%c ", node->flag);
+//}
+//
+//
+//int main()
+//{
+//	binTree node[7];
+//	for (int i = 0; i < 7; i++)
+//	{
+//		node[i].flag = 'A' + i;
+//		node[i].lNode = NULL;
+//		node[i].rNode = NULL;
+//	}
+//	node[0].lNode = &node[1];
+//	node[1].lNode = &node[2];
+//	node[1].rNode = &node[3];
+//	node[0].rNode = &node[4];
+//	node[4].lNode = &node[5];
+//	node[4].rNode = &node[6];
+//
+//	cout << "先序遍历：";
+//	recursionF(node);
+//	putchar(10);
+//
+//	cout << "中序遍历：";
+//	recursionM(node);
+//	putchar(10);
+//
+//	cout << "后序遍历：";
+//	recursionL(node);
+//	putchar(10);
+//
+//	system("pause");
+//	return 0;
+//}
